a2de4b44f8dbacd8ed357abcfe24f376a335c64a
郑志胜 <zhengzhisheng@zhengzhishengdeMacBook-Pro.local>
2017-10-02 22:05:56 +0800

    fix:AES加密解密工具类。

--- sheng-common/src/main/java/com/sheng/common/util/AESUtil.java
+++ sheng-common/src/main/java/com/sheng/common/util/AESUtil.java
@@ -1,5 +1,6 @@
 package com.sheng.common.util;
 
+import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
 import javax.crypto.BadPaddingException;
@@ -9,6 +10,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
@@ -23,6 +25,17 @@ public class AESUtil {
 
     private static final String encodeRules = "zheng";
 
+    /**
+     * * 加密
+     * 1.构造密钥生成器
+     * 2.根据ecnodeRules规则初始化密钥生成器
+     * 3.产生密钥
+     * 4.创建和初始化密码器
+     * 5.内容加密
+     * 6.返回字符串
+     * @param content
+     * @return
+     */
     public static String AESEncode(String content){
         try {
             //1.构造密钥生成器，指定为AES算法,不区分大小写
@@ -69,6 +82,52 @@ public class AESUtil {
         //如果有错就返加nulll
         return null;
     }
+
+    public static String AESDecode(String content){
+        try {
+            //1.构造密钥生成器，指定为AES算法,不区分大小写
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            //2.根据ecnodeRules规则初始化密钥生成器
+            //生成一个128位的随机源,根据传入的字节数组
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+            random.setSeed(encodeRules.getBytes());
+            keygen.init(128, random);
+            //3.产生原始对称密钥
+            SecretKey original_key = keygen.generateKey();
+            //4.获得原始对称密钥的字节数组
+            byte[] raw = original_key.getEncoded();
+            //5.根据字节数组生成AES密钥
+            SecretKey key = new SecretKeySpec(raw, "AES");
+            //6.根据指定算法AES自成密码器
+            Cipher cipher = Cipher.getInstance("AES");
+            //7.初始化密码器，第一个参数为加密(Encrypt_mode)或者解密(Decrypt_mode)操作，第二个参数为使用的KEY
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            //8.将加密并编码后的内容解码成字节数组
+            byte[] byte_content = new BASE64Decoder().decodeBuffer(content);
+            /*
+             * 解密
+             */
+            byte[] byte_decode = cipher.doFinal(byte_content);
+            String AES_decode = new String(byte_decode, "utf-8");
+            return AES_decode;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException("兄弟，配置文件中的密码需要使用AES加密，请使用com.zheng.common.util.AESUtil工具类修改这些值！");
+            //e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        }
+        //如果有错就返加nulll
+        return null;
+    }
+
     public static void main(String[] args) {
         String[] keys = {
                 "", "password"
@@ -78,8 +137,8 @@ public class AESUtil {
             System.out.print(key + " | ");
             String encryptString = AESEncode(key);
             System.out.print(encryptString + " | ");
-//            String decryptString = AESDecode(encryptString);
-//            System.out.println(decryptString);
+            String decryptString = AESDecode(encryptString);
+            System.out.println(decryptString);
         }
     }
 }
